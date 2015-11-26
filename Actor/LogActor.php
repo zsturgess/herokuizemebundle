7d44da3348a4394301686d8f2f06e720c6eb896a
Zac Sturgess <zac.sturgess@wearetwogether.com>
2015-11-26 19:27:56 +0000

    Adding LogActor

--- Actor/LogActor.php
+++ Actor/LogActor.php
@@ -2,6 +2,8 @@
 
 namespace ZacSturgess\HerokuizeMeBundle\Actor;
 
+use Symfony\Component\Yaml\Dumper;
+
 /**
  * LogActor
  */
@@ -10,11 +12,80 @@ class LogActor extends BaseActor
     public function run()
     {   
         // @todo: Check logs go to stderr at least in prod mode
+        $prodConfig = $this->parser->parse(file_get_contents($this->baseDir . 'app/config/config_prod.yml'));
+        
+        $correctHandlers = 0;
+        
+        if (isset($prodConfig['monolog']) && isset($prodConfig['monolog']['handlers'])) {
+            foreach ($prodConfig['monolog']['handlers'] as $handler) {
+                if (
+                    !isset($handler['path']) || 
+                    (
+                        $handler['path'] !== 'php://stderr' &&
+                        strpos($handler['path'], '%') !== 0
+                    )
+                ) {
+                    return '<error>Symfony is configured to always send logs to files.</error> This doesn\'t work on disposable servers like Heroku dynos.';
+                } else {
+                    $correctHandlers++;
+                }
+            }
+        }
+        
+        if ($correctHandlers !== 0) {
+            return true;
+        }
+        
+        $config = $this->parser->parse(file_get_contents($this->baseDir . 'app/config/config.yml'));
+        
+        if (isset($config['monolog']) && isset($config['monolog']['handlers'])) {
+            foreach ($config['monolog']['handlers'] as $handler) {
+                if (
+                    !isset($handler['path']) || 
+                    (
+                        $handler['path'] !== 'php://stderr' &&
+                        strpos($handler['path'], '%') !== 0
+                    )
+                ) {
+                    return '<error>Symfony is configured to always send logs to files.</error> This doesn\'t work on disposable servers like Heroku dynos.';
+                }
+            }
+        }
+        
+        return true;
     }
     
     public function fix()
     {
-        // @todo: Fix config (perhaps in advanced way with php logic)
+        // Our env_paramters.php contains cusotm logic we depend on, so fail if we're not using it
+        if (!$this->fs->exists($this->baseDir . 'app/config/env_parameters.php')) {
+            return '<error>Could not apply auto-fixes without overwriting your own custom changes.</error> It\'s recommended that you redirect logs to php://stderr instead of a file when on Heroku.';
+        }
+        
+        $dumper = new Dumper;
+        
+        foreach (['config.yml', 'config_prod.yml', 'config_dev.yml'] as $configFile) {
+            $config = $this->parser->parse(file_get_contents($this->baseDir . 'app/config/' . $configFile));
+        
+            if (isset($config['monolog']) && isset($config['monolog']['handlers'])) {
+                foreach ($config['monolog']['handlers'] as $handlerKey => $handler) {
+                    if (
+                        !isset($handler['path']) || 
+                        (
+                            $handler['path'] !== 'php://stderr' &&
+                            strpos($handler['path'], '%') !== 0
+                        )
+                    ) {
+                        $config['monolog']['handlers'][$handlerKey]['path'] = '%logging_location%';
+                    }
+                }
+            }
+            
+            file_put_contents(
+                $this->baseDir . 'app/config/' . $configFile,
+                $dumper->dump($config, 4)
+            );
+        }
     }
     
     public function getSuccessMessage()
--- Resources/templates/env_parameters.php
+++ Resources/templates/env_parameters.php
@@ -6,3 +6,11 @@ foreach($container->getParameterBag()->all() as $key => $value) {
         $container->setParameter($key, $envValue);
     }
 }
+
+// Use the DYNO env param to detect if we are on Heroku and redirect logs to stderr
+if (getenv('DYNO') !== false) {
+    $container->setParameter('logging_location', 'php://stderr');
+} else {
+    $location = $container->getParameter('kernel.logs_dir') . '/' . $container->getParameter('kernel.environment') . '.log';
+    $container->setParameter('logging_location', $location);
+}
